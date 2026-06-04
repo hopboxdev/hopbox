766cbfa8bb6b29a9e45b270bf3e6fe5952c53097
Mesa Dev <dev@mesa.local>
2026-06-04 15:58:25 +0000

    feat(cli): mesa create/ls/rm/shell

---
+++ cmd/mesa/main.go
@@ -0,0 +1,108 @@
+// Command mesa is the Mesa user CLI: create/ls/rm/shell against mesad.
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	mesav1 "github.com/mesadev/mesa/gen/mesa/v1"
+)
+
+var apiAddr string
+
+func dial() (mesav1.WorkspaceServiceClient, func(), error) {
+	conn, err := grpc.NewClient(apiAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, nil, err
+	}
+	return mesav1.NewWorkspaceServiceClient(conn), func() { _ = conn.Close() }, nil
+}
+
+func main() {
+	root := &cobra.Command{Use: "mesa", Short: "Mesa dev-environment CLI"}
+	root.PersistentFlags().StringVar(&apiAddr, "addr", "localhost:7700", "mesad API address")
+
+	root.AddCommand(newCreateCmd(), newListCmd(), newRmCmd(), newShellCmd(dial))
+	if err := root.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
+
+func newCreateCmd() *cobra.Command {
+	var image string
+	var mem int64
+	c := &cobra.Command{
+		Use:   "create <name>",
+		Short: "Create a workspace",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
+			client, closer, err := dial()
+			if err != nil {
+				return err
+			}
+			defer closer()
+			w, err := client.CreateWorkspace(context.Background(), &mesav1.CreateWorkspaceRequest{
+				Name: args[0], ImageRef: image, MemMb: mem,
+			})
+			if err != nil {
+				return err
+			}
+			fmt.Printf("created %s (%s) phase=%s\n", w.Name, w.Id, w.Phase)
+			return nil
+		},
+	}
+	c.Flags().StringVar(&image, "image", "ubuntu:24.04", "container image")
+	c.Flags().Int64Var(&mem, "mem-mb", 0, "memory limit in MB (0=unlimited)")
+	return c
+}
+
+func newListCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "ls",
+		Short: "List workspaces",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			client, closer, err := dial()
+			if err != nil {
+				return err
+			}
+			defer closer()
+			resp, err := client.ListWorkspaces(context.Background(), &mesav1.ListWorkspacesRequest{})
+			if err != nil {
+				return err
+			}
+			tw := tabwriter.NewWriter(os.Stdout, 0, 2, 2, ' ', 0)
+			fmt.Fprintln(tw, "NAME\tPHASE\tAGENT\tIMAGE\tID")
+			for _, w := range resp.Workspaces {
+				fmt.Fprintf(tw, "%s\t%s\t%v\t%s\t%s\n", w.Name, w.Phase, w.AgentConnected, w.ImageRef, w.Id)
+			}
+			return tw.Flush()
+		},
+	}
+}
+
+func newRmCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "rm <name|id>",
+		Short: "Destroy a workspace",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
+			client, closer, err := dial()
+			if err != nil {
+				return err
+			}
+			defer closer()
+			if _, err := client.DeleteWorkspace(context.Background(), &mesav1.DeleteWorkspaceRequest{NameOrId: args[0]}); err != nil {
+				return err
+			}
+			fmt.Printf("destroying %s\n", args[0])
+			return nil
+		},
+	}
+}
--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/sys/atomicwriter v0.1.0 // indirect
@@ -35,6 +36,8 @@ require (
 	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.69.0 // indirect
 	go.opentelemetry.io/otel v1.44.0 // indirect
@@ -43,6 +46,7 @@ require (
 	go.opentelemetry.io/otel/trace v1.44.0 // indirect
 	golang.org/x/net v0.55.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
+	golang.org/x/term v0.43.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260526163538-3dc84a4a5aaa // indirect
